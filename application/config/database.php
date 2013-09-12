330468c6adb364936f9b964b003a304fc651ceba
Marcelo Diniz <marcelo.leo27@gmail.com>
2013-09-12 10:49:39 -0300

    correct config

--- application/config/database.php
+++ application/config/database.php
@@ -49,8 +49,8 @@ $active_group = 'default';
 $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
-$db['default']['username'] = 'root';
-$db['default']['password'] = 'm@rc310';
+$db['default']['username'] = '';
+$db['default']['password'] = '';
 $db['default']['database'] = 'codeistrap';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
